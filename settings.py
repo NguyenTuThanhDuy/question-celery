71f550cb99457ba77b60a7977e198bfe674ccccd
Duy Nguyen <duyntt0110@gmail.com>
2023-12-13 17:51:24 +0700

    tmp

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 CELERY_SETTINGS = {
-    'CELERY_ENABLE_UTC': True,
-    'CELERY_RESULT_BACKEND': 'redis://localhost:6379/0',
-    'BROKER_URL': 'redis://localhost:6379/0',
-    'CELERY_SEND_TASK_SENT_EVENT': True,
-    'CELERY_TASK_SERIALIZER': 'pickle',
-    'CELERY_RESULT_SERIALIZER': 'pickle',
-    'CELERY_ACCEPT_CONTENT': ['pickle', 'json'],
+    'broker_connection_retry_on_startup': True,
+    'result_backend': 'redis://localhost:6379/0',
+    'broker_url': 'pyamqp://guest@localhost//',
+    'task_send_sent_event': True,
+    'task_serializer': 'pickle',
+    'result_serializer': 'pickle',
+    'accept_content': ['pickle', 'json'],
 }
